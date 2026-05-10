43261df6defe882cc590af500d7692440ff5e962
Mani Chandan Gujja <152565717+Gujjamanichandan@users.noreply.github.com>
2026-05-10 13:06:54 -0400

    Add files via upload

---
+++ java course/Hello.java
@@ -0,0 +1,104 @@
+// class Calculator{
+    
+//     public int add(int n1, int n2)
+//     {
+//       int r = n1 + n2 ; 
+//         return r;
+//     }
+
+//     public void playMusic(){
+//         System.out.println("Playing music");
+//     }
+
+//     public String pen(int a)
+//     {
+//         if(a>=10)
+//           return "pen";
+        
+//         return "Nothing";
+
+//     }
+// }
+
+
+
+
+
+class Hello
+{
+
+   
+    public static void main(String a[]) {
+        //  int num1 = 1;
+        // int num2 = 2;
+        // Calculator cal = new Calculator();
+
+        // int result = cal.add(num1,num2);
+
+        // System.out.println(result);
+
+        // cal.playMusic();
+
+        // String srt = cal.pen(9);
+        // System.out.println(srt);
+
+        //Array ---------------------------------------------------------------------------------------
+
+        // int num[] = new int[4];
+
+        // for(int i=0;i<5;i++){
+        //     num[i]=i;
+        //     System.out.println(num[i]);
+        // }
+        
+        //Two dimentional Array----------------------------------------------------------------------------
+ 
+        // int num [][] = new int[3][4];
+
+        // for (int i=0;i<=2;i++)
+        //     {
+        //     for(int j=0;j<=3;j++){
+        //         num[i][j]=(int)(Math.random()*10);
+        //         System.out.print(num[i][j] + " ");
+        //     }
+        //     System.out.println("");
+        // }
+
+        //enhanced for loop method-----------------------------------------------------------------------------
+
+        // for(int n[]: num){
+        //     for(int m : n){
+        //         System.err.print(m +" ");
+        //     }System.out.println();
+        // }
+
+       // Jagged array-----------------------------------------------------------------------------------------
+
+    //    int num [][] = new int[3][];
+    //     num [0] = new int[3];
+    //     num [1] = new int[4];
+    //     num [2] = new int[2];
+
+    //     for(int i=0;i<num.length;i++)
+    //         {
+    //         for(int j=0;j<num[i].length;j++){
+    //             num[i][j]=(int)(Math.random()*10);
+    //             System.out.print(num[i][j] + " ");
+    //         }
+    //         System.out.println("");
+    //     }
+        
+    //     //enhanced (for enchanced loop we dont need to mention the length it will automatically take the length)
+    //     for(int n[]:num){
+    //         for(int m:n)
+    //             {
+    //              System.out.print(m+" ");
+    //         }System.out.println();
+    //     }
+
+    
+
+    }
+}
+
+
