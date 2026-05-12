575a2a8aa41a13321996dde303d16d793f5c9351
gujja mani chandan <gujjamanichandan@gmail.com>
2026-05-12 19:59:33 -0400

    encapsulation

--- java_course/Hello.java
+++ java_course/Hello.java
@@ -26,6 +26,34 @@ class Student{
     int marks;
 }
 
+class Human{
+    private int age;
+    private String name;
+
+    public Human()
+    {
+        name = "shiva";
+        age = 20;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+}
+
 
 
 class Hello
@@ -103,31 +131,39 @@ class Hello
     //     }
 
     //Arrey of objests form class Student----------------------------------------------------------------------------------------
-        Student s1 = new Student();
-        s1.name = "chandu";
-        s1.rollno = 1;
-        s1.marks = 100;
+    //     Student s1 = new Student();
+    //     s1.name = "chandu";
+    //     s1.rollno = 1;
+    //     s1.marks = 100;
 
-        Student s2 = new Student();
-        s2.name = "shiva";
-        s2.rollno = 2;
-        s2.marks = 100;
+    //     Student s2 = new Student();
+    //     s2.name = "shiva";
+    //     s2.rollno = 2;
+    //     s2.marks = 100;
 
-        Student s3 = new Student();
-        s3.name = "Bannu";
-        s3.rollno = 3;
-        s3.marks = 100;
+    //     Student s3 = new Student();
+    //     s3.name = "Bannu";
+    //     s3.rollno = 3;
+    //     s3.marks = 100;
 
-    Student students[] = new Student[3];
-        students[0] = s1;
-        students[1] = s2;
-        students[2] = s3;
+    // Student students[] = new Student[3];
+    //     students[0] = s1;
+    //     students[1] = s2;
+    //     students[2] = s3;
 
-        //enhanced for loop
-       for (Student stud : students) {
+    //     //enhanced for loop
+    //    for (Student stud : students) {
            
-         System.out.println(stud.name + ":" + stud.marks);
-       }
+    //      System.out.println(stud.name + ":" + stud.marks);
+    //    }
+
+    //Encapsulation-----------------------------------------------------------------------------------------------
+    Human obj = new Human();
+
+    // obj.setName("chandu");
+    // obj.setAge(24);
+
+    System.out.println(obj.getName()  + " : " +  obj.getAge());
 
 
     }
